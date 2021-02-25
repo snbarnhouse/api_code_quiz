72a6b1138e98c87f78e09861809572b78fb33317
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-02-25 13:24:43 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,65 @@
+//what question # are they at?
+var questionNumber = 0;
+//answered correctly
+var correct = 0;
+//answered incorrectly
+var incorrect = 0;
+//questions
+var questions = [
+    {
+        question: "Choose the correct HTML tag for the largest heading.",
+        A: "h1",
+        B: "heading",
+        C: "head",
+        D: "title",
+        Answer: "A",
+    },
+    {
+        question: "What CSS selector defines a style for a unique element?",
+        A: "class",
+        B: "id",
+        C: "name",
+        D: "attribute",
+        Answer: "B",
+    },
+    {
+        question: "What type of language is Javascript?",
+        A: "Markup",
+        B: "Scripting",
+        C: "Programming",
+        D: "All of the above.",
+        Answer: "B",
+    },
+    {
+        question: "What is the term for text that automatically continues from one line to the next?",
+        A: "Word flowing",
+        B: "Word binding",
+        C: "Word wrapping",
+        D: "Word processing",
+        Answer: "C",
+    },
+    {
+        question: "Approximately, how many much data exists in the digital universe?",
+        A: "2.7 terabytes",
+        B: "2.7 zetabytes",
+        C: "2.7 gigabytes",
+        D: "2.7 megabytes",
+        Answer: "B",
+    },
+    {
+        question: "Guido Van Rossum the creator of Python named the language after what?",
+        A: "the snake",
+        B: "a rock band",
+        C: "Monty Python",
+        D: "None of the above.",
+        Answer: "C",
+    },
+    {
+        question: "According to recent data from the Bureau of Labor Statisics women account for ____ of computer science jobs.",
+        A: "15%",
+        B: "9%",
+        C: "37%",
+        D: "24%",
+        Answer: "D",
+    }
+];
